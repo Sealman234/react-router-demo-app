3930128dfc2430fed0fc40473b19ec1cbce64112
Sealman <castle2668@gmail.com>
2022-02-14 17:04:46 +0800

    Adding the Comments Features

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,14 +1,55 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import classes from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
+import CommentsList from './CommentsList';
+
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
+import LoadingSpinner from '../UI/LoadingSpinner';
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
 
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+  // 取得該 Quote 的所有留言
+  const { quoteId } = params;
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  // 展開留言輸入欄
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
+
+  // 新增留言完成後，再次取得所有留言
+  const addedCommentHandler = useCallback(() => {
+    sendRequest(quoteId);
+    setIsAddingComment(false);
+  }, [sendRequest, quoteId]);
+
+  // 顯示評論清單
+  let comments;
+  if (status === 'pending') {
+    comments = (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+  if (status === 'completed' && loadedComments && loadedComments.length > 0) {
+    comments = <CommentsList comments={loadedComments} />;
+  }
+  if (
+    status === 'completed' &&
+    (!loadedComments || loadedComments.length === 0)
+  ) {
+    comments = <p className="centered">No comments were added yet!</p>;
+  }
 
   return (
     <section className={classes.comments}>
@@ -18,8 +59,11 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && (
+        <NewCommentForm onAddComment={addedCommentHandler} quoteId={quoteId} />
+      )}
+      {/* <p>Comments...</p> */}
+      {comments}
     </section>
   );
 };
--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,20 +1,47 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
+
+import LoadingSpinner from '../UI/LoadingSpinner';
 
 import classes from './NewCommentForm.module.css';
+
+import useHttp from '../../hooks/use-http';
+import { addComment } from '../../lib/api';
 
 const NewCommentForm = (props) => {
   const commentTextRef = useRef();
 
+  const { sendRequest, status, error } = useHttp(addComment);
+
+  // 送出留言後，告訴 Comments 讓它重新取得所有留言
+  const { onAddComment } = props;
+  useEffect(() => {
+    if (status === 'completed' && !error) {
+      onAddComment();
+    }
+  }, [status, error, onAddComment]);
+
+  // 送出留言
   const submitFormHandler = (event) => {
     event.preventDefault();
 
     // optional: Could validate here
+    const enteredText = commentTextRef.current.value;
+    if (enteredText.trim() === '') return;
 
     // send comment to server
+    sendRequest({
+      commentData: { text: enteredText },
+      quoteId: props.quoteId,
+    });
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === 'pending' && (
+        <div>
+          <LoadingSpinner />
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
         <label htmlFor="comment">Your Comment</label>
         <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
